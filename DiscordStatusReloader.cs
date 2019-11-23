e82fc16141ae884723cff91460bbfd6f5ef7e71b
oryxcoding <51522724+oryxcoding@users.noreply.github.com>
2019-11-23 19:49:45 +0400

    Add files via upload

---
+++ DiscordStatusReloader.cs
@@ -0,0 +1,25 @@
+﻿using Oxide.Core.Plugins;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oxide.Plugins
+{
+    [Info("Discord Status Reloader", "Oryx", "1.0.0")]
+    [Description("Reloads Discord Status when its stops responding")]
+
+    public class DiscordStatusReloader : RustPlugin
+    {
+        private float reload_in_seconds = 1800;
+
+        void Init()
+        {
+            timer.Every(reload_in_seconds, () =>
+            {
+                rust.RunServerCommand("o.reload DiscordStatus");
+            });
+        }
+    }
+}
